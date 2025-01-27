2e00469f2e8677a18ef6f82794894013a8a5cc80
Kosma Kierek <kosma.kierek@host.com>
2025-01-27 03:04:32 +0100

    af

--- WarhinczykAI.py
+++ WarhinczykAI.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template, render_template_string, redirect, request, session, redirect, url_for
+from flask import Flask, Blueprint, render_template, render_template_string, redirect, request, session, redirect, url_for
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from WarhinczykAI import WarhinczykAI
 import random
 
 #app setup
-Warhinczyk = Flask(__name__)
-Warhinczyk.register_blueprint(WarhinczykAI, url_prefix="")
-Scss(Warhinczyk)
+WarhinczykAI = Blueprint("WarhinczykAI", __name__, static_folder="static", template_folder="templates")
+#Scss(WarhinczykAI)
 
-Warhinczyk.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+#WarhinczykAI.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-Warhinczyk.secret_key = 'BAD_SECRET_KEY'
+#WarhinczykAI.secret_key = 'BAD_SECRET_KEY'
 #-------------------------------------------------------------------------
 class Game:
     def __init__(self, field):
@@ -37,7 +35,7 @@ pionki = {
     6: player2.pionki[2]
 }
 
-@Warhinczyk.route("/game", methods=["POST","GET"])
+@WarhinczykAI.route("/gameAI", methods=["POST","GET"])
 def game():
     if request.method == "GET":
         fields.field=[0,0,0,0,0,0,0,0,0,0]
@@ -45,10 +43,10 @@ def game():
         player2.current=0
         player1.score=0
         player2.score=0
-    return render_template('gra/game.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+    return render_template('graAI/game.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
 #rzut kostka, ruchy w /game/<costam>
 
-@Warhinczyk.route("/game/p1", methods=["POST","GET"])
+@WarhinczykAI.route("/gameAI/p1", methods=["POST","GET"])
 def movep1():
     if request.method == "POST":
         for i in fields.field:
@@ -69,27 +67,27 @@ def movep1():
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player1.current+now>=len(fields.field):
             player1.score+=10
             if player1.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player2.current>=len(fields.field):
             player2.score+=10
             if player2.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player1.current+now<len(fields.field):
             if (fields.field[player1.current+now]==fields.field[player2.current] and player2.current!=0):
                 fields.field[player1.current]=0
@@ -98,9 +96,9 @@ def movep1():
                 fields.field[player1.current+now]=pionki[1]
                 fields.field[player1.current]=0
                 player1.current=player1.current+now
-    return render_template('gra/gamep2_1.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
+    return render_template('graAI/gamep2_1.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
 
-@Warhinczyk.route("/game/p2", methods=["POST","GET"])
+@WarhinczykAI.route("/gameAI/p2", methods=["POST","GET"])
 def movep2():
     if request.method == "POST":
         for i in fields.field:
@@ -121,27 +119,27 @@ def movep2():
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player1.current+now>=len(fields.field):
             player1.score+=10
             if player1.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player2.current>=len(fields.field):
             player2.score+=10
             if player2.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player1.current+now<len(fields.field):
             if (fields.field[player1.current+now]==fields.field[player2.current] and player2.current!=0):
                fields.field[player2.current]=0
@@ -153,9 +151,9 @@ def movep2():
                fields.field[player1.current+now]=pionki[2]
                fields.field[player1.current]=0
                player1.current=player1.current+now
-    return render_template('gra/gamep2_2.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
+    return render_template('graAI/gamep2_2.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
 
-@Warhinczyk.route("/game/p3", methods=["POST","GET"])
+@WarhinczykAI.route("/gameAI/p3", methods=["POST","GET"])
 def movep3():
     if request.method == "POST":
         for i in fields.field:
@@ -176,27 +174,27 @@ def movep3():
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player1.current+now>=len(fields.field):
             player1.score+=10
             if player1.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player2.current>=len(fields.field):
             player2.score+=10
             if player2.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player1.current+now<len(fields.field):
             if (fields.field[player1.current+now]==fields.field[player2.current] and player2.current!=0):
                 fields.field[player1.current]=0
@@ -206,11 +204,11 @@ def movep3():
                 fields.field[player1.current+now]=pionki[3]
                 fields.field[player1.current]=0
                 player1.current=player1.current+now
-    return render_template('gra/gamep2_3.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
+    return render_template('graAI/gamep2_3.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
 
 #----------------------------------------------------------------------------------
 
-@Warhinczyk.route("/game/p4", methods=["POST","GET"])
+@WarhinczykAI.route("/gameAI/p4", methods=["POST","GET"])
 def movep4():
     if request.method == "POST":
         for i in fields.field:
@@ -228,27 +226,27 @@ def movep4():
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player2.current+now>=len(fields.field):
             player2.score+=10
             if player2.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player1.current>=len(fields.field):
             player1.score+=10
             if player1.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player2.current+now<len(fields.field):
             if (fields.field[player2.current+now]==fields.field[player1.current] and player1.current!=0):
                 fields.field[player2.current]=0
@@ -257,9 +255,9 @@ def movep4():
                 fields.field[player2.current+now]=pionki[4]
                 fields.field[player2.current]=0
                 player2.current=player2.current+now
-    return render_template('gra/gamep1_1.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
+    return render_template('graAI/gamep1_1.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
 
-@Warhinczyk.route("/game/p5", methods=["POST","GET"])
+@WarhinczykAI.route("/gameAI/p5", methods=["POST","GET"])
 def movep5():
     if request.method == "POST":
         for i in fields.field:
@@ -277,27 +275,27 @@ def movep5():
                player1.current=0
                player2.current=0
                player2.score+=10
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player2.current+now>=len(fields.field):
             player2.score+=10
             if player2.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player1.current>=len(fields.field):
             player1.score+=10
             if player1.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player2.current+now<len(fields.field):
             if (fields.field[player2.current+now]==fields.field[player1.current] and player1.current!=0):
                 fields.field[player1.current]=0
@@ -309,9 +307,9 @@ def movep5():
                 fields.field[player2.current+now]=pionki[5]
                 fields.field[player2.current]=0
                 player2.current=player2.current+now
-    return render_template('gra/gamep1_2.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
+    return render_template('graAI/gamep1_2.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
 
-@Warhinczyk.route("/game/p6", methods=["POST","GET"])
+@WarhinczykAI.route("/gameAI/p6", methods=["POST","GET"])
 def movep6():
     if request.method == "POST":
         for i in fields.field:
@@ -329,27 +327,27 @@ def movep6():
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player2.current+now>=len(fields.field):
             player2.score+=10
             if player2.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P2win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/loose.html')
+               return render_template('graAI/loose.html')
         elif player1.current>=len(fields.field):
             player1.score+=10
             if player1.score<30:
                fields.field=[0,0,0,0,0,0,0,0,0,0]
                player1.current=0
                player2.current=0
-               return render_template('gra/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
+               return render_template('graAI/P1win.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score)
             else:
-               return render_template('gra/win.html')
+               return render_template('graAI/win.html')
         elif player2.current+now<len(fields.field):
             if (fields.field[player2.current+now]==fields.field[player1.current] and player1.current!=0):
                 fields.field[player2.current]=0
@@ -359,7 +357,7 @@ def movep6():
                 fields.field[player2.current+now]=pionki[6]
                 fields.field[player2.current]=0
                 player2.current=player2.current+now
-    return render_template('gra/gamep1_3.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
+    return render_template('graAI/gamep1_3.html',fields=fields.field,pionki=pionki,P1score=player1.score,P2score=player2.score,now=now)
 
 
 #@Warhinczyk.route("/game", methods=["POST","GET"])
@@ -374,5 +372,5 @@ def movep6():
 #   return render_template('game.html', player1=player1, current_field=current_field, pola=pola )
 
 
-if __name__ in "__main__":
-    Warhinczyk.run(debug=True)
+#if __name__ in "__main__":
+ #   WarhinczykAI.run(debug=True)
